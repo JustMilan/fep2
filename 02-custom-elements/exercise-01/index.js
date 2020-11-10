104d8625dfd856a129a29d6ba83e8644a37c903d
JustMilan <milandol321@gmail.com>
2020-11-10 16:21:26 +0100

    Add elaboration 02-exercise-01
    Added my elaboration of exercise-01 of 02-custom-elements
    
    Took 1 hour 8 minutes

---
+++ 02-custom-elements/exercise-01/index.js
@@ -0,0 +1,31 @@
+class Switch extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const lightBulbDiv = document.createElement('div');
+        lightBulbDiv.className = "light-switch";
+        lightBulbDiv.id = "bulb";
+        const lightBulbButton = document.createElement('button');
+        lightBulbButton.className = "switch";
+        lightBulbButton.textContent = "Turn on/off"
+        lightBulbButton.id = "lightBulbButton";
+        this.appendChild(lightBulbDiv)
+        this.appendChild(lightBulbButton)
+        lightBulbButton.addEventListener("click", this.changeColor)
+    }
+
+    changeColor() {
+        // Door wat moet ik document.queryselector vervangen om this te kunnen gebruiken?
+        const bulb = document.querySelector('#bulb')
+        if (bulb.style.backgroundColor === "grey") {
+            bulb.style.backgroundColor = "yellow";
+        } else {
+            bulb.style.backgroundColor = "grey"
+        }
+    }
+
+}
+
+window.customElements.define('light-switch', Switch)
